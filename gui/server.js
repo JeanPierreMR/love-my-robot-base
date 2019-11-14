96fba9693a66a6483c329a1c5300d67c8ee8bea8
JBarillas <jbarillas2015ig@gmail.com>
2019-11-14 14:48:12 -0600

    HTML sendFile try
    Failed res.sendFile

--- gui/server.js
+++ gui/server.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => res.send('Hello From Express'))
-
+app.get('/prueba',(req, res) => res.sendFile('prueba.html'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
