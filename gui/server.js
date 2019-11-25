563bde633e2ad214396612b10b97f3b96369c3c0
Adriana Mundo <adrianamundog@hotmail.com>
2019-11-25 12:14:36 -0600

    GUI update

--- gui/server.js
+++ gui/server.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express")
+const path = require ("path")
 
-app.get('/', (req, res) => res.send('Hello From Express'))
+const app = express();
+const port = process.env.PORT || "8000";
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+/**
+ * Routes Definitions
+ */
+app.get("/", (req,res) => {
+    //res.status(200).send("Whatabyte: Food for devs")
+    res.render("index", {title: "Home"})
+});
+app.get("/user", (req, res) => {
+    res.render("user", {title: "Profile", userProfile: {nickname: "Auth0"}})
+});
+
+
+app.listen(port,  ()=> {
+console.log('Listening to requests on http://localhost:${port}');
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
