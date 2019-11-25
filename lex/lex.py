f5a7ebe4a05d6f4bc474551564d1d193e44e0d75
Jean <jpmejicanos@ufm.edu>
2019-11-25 15:02:53 -0600

    Update lex.py
    added routes to flask

--- lex/lex.py
+++ lex/lex.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request 
 
 app = Flask(__name__)
 
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return "Hello from Flask!"
 
+@app.route("/compile", methods = ['GET', 'POST'])
+def compilation():
+    request.form.get('subir') 
+    pass
+
+@app.route("/execute", methods = ['GET', 'POST'])
+def compilation():
+    pass
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
